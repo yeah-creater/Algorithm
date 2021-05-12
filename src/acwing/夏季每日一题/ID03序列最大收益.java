9e1ab2baf996e02417f4b6081fb4822f9e8834e2
ybz <1615498405@qq.com>
2021-05-12 22:31:30 +0800

    algorithm

--- src/acwing/夏季每日一题/ID03序列最大收益.java
+++ src/acwing/夏季每日一题/ID03序列最大收益.java
@@ -9,8 +9,8 @@ public class ID03序列最大收益 {
     static int n, k, m;
     static int[][] w = new int[210][210];
     static int[] q = new int[210];
-    //前i个数中去掉了j个数，且保留第i中数的所有方案中的最大收益
-    static int[][]f=new int[210][210];
+    //前i个数中去掉了j个数，且保留第i个数的所有方案中的最大收益
+    static int[][] f = new int[210][210];
 
 
     public static void main(String[] args) {
@@ -26,15 +26,20 @@ public class ID03序列最大收益 {
                 w[i][j] = in.nextInt();
             }
         }
-        int res=0;
-        for(int i=1;i<m;i++){
-            res+=w[q[i]][q[i+1]];
-        }
-        System.out.println(res);
-        for(int i=1;i<=m;i++){
-            for(int j=0;j<=k;j++){
 
+        int res = 0;
+        for (int i = 2; i <= m; i++) {
+            for (int j = 1; j <= k; j++) {
+                for (int u = 1; u < i; u++) {
+                    if (j >= i - u + 1) {
+                        f[i][j] = Math.max(f[i][j], f[i - u][j - (i - u - 1)] + w[q[u]][q[i]]);
+                    }
+                }
             }
         }
+        for (int i = 0; i < k; i++) {
+            res = Math.max(res, f[m][i]);
+        }
+        System.out.println(res);
     }
 }
