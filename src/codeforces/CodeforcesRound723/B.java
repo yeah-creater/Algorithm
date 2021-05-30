8883ab67851bcc68fa7b31c3466e063662b45525
ybz <1615498405@qq.com>
2021-05-30 20:03:31 +0800

    algorithm

--- src/codeforces/CodeforcesRound723/B.java
+++ src/codeforces/CodeforcesRound723/B.java
@@ -1,49 +1,44 @@
 package codeforces.CodeforcesRound723;
 
+import javax.xml.transform.Source;
 import java.util.Scanner;
 
 /**
  * @author: yeah
+ * 1111 = 11 *100 + 11
+ * 11111 = 111*100 + 11
+ * 111111 = 1111 *100 +11
+ * ...
+ * 所以大于 111的数都可以用 a*111+b*11 表示
+ * <p>
+ * 所以 x= 11a+111b   a>=0&&b>=0
+ * x= 11(a+10b)+b
+ * x%11 = b  b<11
+ * <p>
+ * x-111b=11a
+ * (x-111b)/11 = a
+ * <p>
+ * 1.x%11 = b
+ * 2.a = (x-111b)/11
  */
 public class B {
     static int n;
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        int[] val = {11, 111, 1111, 11111, 111111, 1111111, 11111111, 111111111};
         n = in.nextInt();
 
         while (n-- > 0) {
             int x = in.nextInt();
-            for (int i = 0; i < 1; i++) {
-                boolean t = false;
-                for (int k = 0; k < 7; k++) {
-                    if (x % val[k] == 0) {
-                        System.out.println("YES");
-                        t = true;
-                        break;
-                    }
-
-                }
-                if (!t) {
-                    int cnt = x / val[0];
-                    boolean flag = false;
-                    for (int j = 7; j >= 0; j--) {
-                        while (cnt * 11 >= val[j]) {
-                            x -= val[j];
-                            cnt = x / 11;
-                            if (x % 11 == 0) {
-                                flag = true;
-                                break;
-                            }
-                        }
-                    }
-                    if (flag) System.out.println("YES");
-                    else System.out.println("NO");
-
-
-                }
+            int a = 0, b = 0;
+            b = x % 11;
+            a = (x - 111 * b) / 11;
+            if (a >= 0) {
+                System.out.println("YES");
+            } else {
+                System.out.println("NO");
             }
+
 
         }
     }
